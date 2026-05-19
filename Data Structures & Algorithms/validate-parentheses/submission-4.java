03b6197bb7ad9c37f96b4c4b9f0ada26936df5d9
Brian Chang <brimatt062495@gmail.com>
2026-05-19 08:12:45 -0400

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> openToClose = new HashMap<>();
+
+        openToClose.put(')', '(');
+        openToClose.put(']', '[');
+        openToClose.put('}', '{');
+        
+        for (char c : s.toCharArray()) {
+            if (openToClose.containsKey(c)) {
+                if (!stack.isEmpty() && stack.peek() == openToClose.get(c)) {
+                    stack.pop();
+                }
+                else {
+                    return false;
+                }
+            }
+            else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
