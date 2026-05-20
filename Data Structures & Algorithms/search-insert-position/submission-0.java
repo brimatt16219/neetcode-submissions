588c9148cf044a690e1aa97d91a98d79a10f86f2
Brian Chang <brimatt062495@gmail.com>
2026-05-20 19:38:08 -0400

    Add: search-insert-position - submission-0

---
+++ Data Structures & Algorithms/search-insert-position/submission-0.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+        int res = nums.length;
+        while (l <= r) {
+            int m = l + ((r - l) / 2);
+            
+            if (nums[m] == target) {
+                return m;
+            }
+            if (nums[m] > target) {
+                res = m;
+                r = m - 1;
+            }
+            else {
+                l = m + 1;
+            }
+        }
+        return res;
+    }
+}
