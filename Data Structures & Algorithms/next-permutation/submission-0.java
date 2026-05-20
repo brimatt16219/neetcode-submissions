8f87f06ff3c3c09aea28c61188af431d29c3a050
Brian Chang <brimatt062495@gmail.com>
2026-05-20 16:26:34 -0400

    Add: next-permutation - submission-0

---
+++ Data Structures & Algorithms/next-permutation/submission-0.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int i = n - 2; 
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = n - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        int l = i + 1;
+        int r = n - 1;
+        while (l < r) {
+            swap(nums, l++, r--);
+        }
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
