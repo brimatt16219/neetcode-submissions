e6e5d2b3dda83b6935deae452addf89a9fed9407
Brian Chang <brimatt062495@gmail.com>
2026-05-24 09:16:07 -0400

    Add: jump-game-ii - submission-1

---
+++ Data Structures & Algorithms/jump-game-ii/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int jump(int[] nums) {
+        Map<Integer, Integer> memo = new HashMap<>();
+        return dfs(nums, 0, memo);
+    }
+
+    public int dfs(int[] nums, int i, Map<Integer, Integer> memo) {
+        if (memo.containsKey(i)) {
+            return memo.get(i);
+        }
+        if (i == nums.length - 1) {
+            return 0;
+        }
+        if (nums[i] == 0) {
+            return 100000;
+        }
+        int res = 100000;
+        int end = Math.min(nums.length, i + nums[i] + 1);
+        for (int j = i + 1; j < end; j++) {
+            res = Math.min(res, 1 + dfs(nums, j, memo));
+        }
+        memo.put(i, res);
+        return res;
+    }
+}
